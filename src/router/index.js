e3c99bd0188af47081f7fa3b7759d25765ef3dba
Nobi <nobi@nobidev.com>
2022-05-31 15:51:53 +0000

    WIP

--- src/router/index.js
+++ src/router/index.js
@@ -9,24 +9,27 @@ import WithoutLeftSidebar from "../layouts/dashboard/without-leftsidebar";
 import WithoutRightSidebar from "../layouts/dashboard/without-rightsidebar";
 import Layout1 from "../layouts/dashboard/layout-1";
 import Simple from "../layouts/dashboard/simple";
+import RouterMiddleware from "./middleware";
 
 const IndexRouters = () => {
   return (
     <>
       <Switch>
-        <Route
-          path="/without-leftsidebar"
-          component={WithoutLeftSidebar}
-        ></Route>
-        <Route
-          path="/without-rightsidebar"
-          component={WithoutRightSidebar}
-        ></Route>
-        <Route path="/auth" component={Simple}></Route>
-        <Route path="/errors" component={Simple}></Route>
-        <Route path="/extra-pages" component={Simple}></Route>
-        <Route path="/dashboards" component={Layout1}></Route>
-        <Route path="/" component={Default}></Route>
+        <RouterMiddleware>
+          <Route
+            path="/without-leftsidebar"
+            component={WithoutLeftSidebar}
+          ></Route>
+          <Route
+            path="/without-rightsidebar"
+            component={WithoutRightSidebar}
+          ></Route>
+          <Route path="/auth" component={Simple}></Route>
+          <Route path="/errors" component={Simple}></Route>
+          <Route path="/extra-pages" component={Simple}></Route>
+          <Route path="/dashboards" component={Layout1}></Route>
+          <Route path="/" component={Default}></Route>
+        </RouterMiddleware>
       </Switch>
     </>
   );
